d018687efdf225a7bcbdedcdb8b9c4fe0f4f62c6
Sammy Wu <swucode@gmail.com>
2018-05-16 05:29:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Neighborhood {
+  constructor(name) {
+    this.id = ++neighborhoods;
+    this.name = name;
+  }
+
+  deliveries() {
+
+  }
+
+  customers() {
+
+  }
+
+  meals() {
+
+  }
+
+}
